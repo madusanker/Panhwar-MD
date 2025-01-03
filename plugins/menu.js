ceba25d3f5447d6bebde0df383a34a59aa8efaf4
ᴀɴsᴀʀ-ᴘᴀɴʜᴡᴀʀ <panhwarking003@gmail.com>
2025-01-03 16:50:28 +0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -45,7 +45,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -115,7 +115,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -190,7 +190,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -246,7 +246,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -302,7 +302,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -353,7 +353,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -412,7 +412,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -461,7 +461,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -514,7 +514,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
@@ -564,7 +564,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://i.ibb.co/59675LG/Manul-Ofc-X.jpg` },
+                image: { url: `https://i.imgur.com/UfzyhWN.jpeg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
